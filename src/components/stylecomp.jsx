6d8dc7fc6fac43bf32fcea1247e31b156b3c4ab8
PoojaVeera <110709890+PoojaVeera@users.noreply.github.com>
2022-08-19 10:01:48 +0530

    modified:   src/App.js 	new file:   src/components/stylecomp.js 	new file:   src/components/stylecomp.jsx

---
+++ src/components/stylecomp.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  HEAD,
+  Immg,
+  FORM,
+  FOOT,
+  BUTTONSI,
+  BUTTONF,
+  BUTTONG,
+  SIN,
+  INPUTBOX,
+} from "./stylecomp.js";
+
+function Mypage() {
+  return (
+    <div>
+      <HEAD>header</HEAD>
+      <Immg
+        src="https://img.freepik.com/premium-vector/woman-working-computer-colorful-vector-concept-cartoon-flat-style-illustration-office-clerk-happy-girl-sitting-desk-with-laptop-female-character-freelancer-secretary-businesswoman_311563-586.jpg?w=2000"
+        alt="welcome"
+      />
+
+      <FORM>
+        <SIN>SIGN IN</SIN>
+        <br />
+        <form>
+          <label>Email address</label>
+          <br />
+          <INPUTBOX>
+            <input type="email" placeholder="yourname@yourmail.com" />
+          </INPUTBOX>
+          <br />
+          <p>Password</p>
+          <INPUTBOX>
+            <input type="password" placeholder="enter your password"></input>
+          </INPUTBOX>
+          <br />
+        </form>
+        <br />
+        <BUTTONSI>SIGN IN</BUTTONSI>
+        <br />
+        <h4>------------------------------OR----------------------------</h4>
+        <BUTTONG>Sign in with Google</BUTTONG>
+        <br />
+        <BUTTONF>Sign in with facebook</BUTTONF>
+        <br />
+      </FORM>
+      <FOOT>Page created by pooja veeranki</FOOT>
+    </div>
+  );
+}
+export default Mypage();
